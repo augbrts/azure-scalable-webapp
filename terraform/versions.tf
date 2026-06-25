12b1095ed382635c5413ebd7ff94fb781a67f8f9
Augusto Bretas <augbrts@gmail.com>
2026-06-25 19:17:09 -0300

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -10,7 +10,6 @@ terraform {
 }
 
 provider "azurerm" {
-  # Obrigatório no azurerm 4.x. Pode vir daqui (var) ou da env ARM_SUBSCRIPTION_ID.
   subscription_id = var.subscription_id
   features {}
 }
